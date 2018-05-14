464bc0cff7e6c26023dec457b5f764ea7ec9356e
BhaskarMuthineni <36664657+BhaskarMuthineni@users.noreply.github.com>
2018-05-14 17:19:34 +0530

    requests session creation

--- Controller/updateToSapVRN.py
+++ Controller/updateToSapVRN.py
@@ -5,8 +5,12 @@ class updateToSapVRN:
         self.db = db;
     
     def postSAPData(self, url, pData):
-        response = requests.get(hstURL+'/Z_FIORI_VRN_IN_LITE_SRV', headers = {"x-csrf-token": "Fetch"})
-        requests.post(hstURL+url, headers = {"x-csrf-token": response.headers["x-csrf-token"]}, data = pData)
+        client = requests.session();
+        response = client.get(hstURL+'/Z_FIORI_VRN_IN_LITE_SRV', headers = {"x-csrf-token": "Fetch"})
+        token = response.headers["x-csrf-token"]
+        headers = {"X-CSRF-Token": token}
+        r = client.post(hstURL+url, json = pData, headers = headers)
+        r.raise_for_status()
     
     def createLicense(self, data):
         LICDta = {
@@ -14,7 +18,7 @@ class updateToSapVRN:
             "LicenceNum": data["Licencenumber"],
             "MobileNum": data["Telephone"],
             "RegionCode": data["Rg"],
-            "ValidUpToDate": data["Validto"][:-4] # need to check validto date senario what it is passing
+            "ValidUpToDate": data["Validto"]#[:-4] # need to check validto date senario what it is passing
             }
         self.postSAPData('/Z_FIORI_VRN_IN_LITE_SRV/LicenceCreateSet', LICDta)
     
@@ -78,145 +82,3 @@ class updateToSapVRN:
             "VRNNum": str(vrn)
             }
         updateToSapVRN.postSAPData('/Z_FIORI_VRN_IN_LITE_SRV/CheckInSet', CheckData);
-        
-    
-    
-# 
-# 
-# 'use strict';
-# var mongoose = require('mongoose'),
-#   request = require('request'),
-#   
-# 
-# function doCall(url, pData) {
-#   var token;
-#   var j = request.jar();
-# 
-#   var postDataToSAP = function () {
-#     return new Promise(function (resolve, reject) {
-#       request({
-#         url: hstURL+"/Z_FIORI_VRN_IN_LITE_SRV",
-#         jar: j,
-#         headers: {
-#           "x-csrf-token": "Fetch"
-#         }
-#       }, function (error, response, body) {
-#         try {
-#           token = response.headers["x-csrf-token"];
-#           console.log("token csrf " + token);
-# 
-#           request({
-#             url: hstURL + url,
-#             method: 'POST',
-#             jar: j,
-#             headers: {
-#               "Content-Type": "application/json",
-#               "X-CSRF-Token": token, // set CSRF Token for post or update
-#             },
-#             json: pData
-#           }, function (error, response, body) {
-# 
-#             console.log("error " );
-#             console.log(error);
-#             console.log("response " );
-#             console.log(response);
-#             console.log("body ");
-#             console.log(body);
-# 
-#             resolve();
-#           });
-#         } catch (err) {
-#           console.log("Cannot post data to sap in catch" + err);
-#         }
-#       });
-# 
-#     });
-#   }
-#   postDataToSAP();
-# }
-# 
-# function postSAPData(pth, data) {
-#   //fetchCSRFToken(pth, data);
-#   doCall(pth, data);
-# }
-# 
-# //0000-00-00T00:00:00
-# function dateFormate(myDate) {
-#   return myDate.substring(0, myDate.length - 5);
-# }
-# 
-# exports.createLicense = function (data) {
-# 
-#   var LICDta = {
-#     DriverName: data.Lastname,
-#     LicenceNum: data.Licencenumber,
-#     MobileNum: data.Telephone,
-#     RegionCode: data.Rg,
-#     ValidUpToDate: dateFormate(data.Validto)
-#   }
-#   postSAPData('/Z_FIORI_VRN_IN_LITE_SRV/LicenceCreateSet', LICDta);
-# 
-# }
-# 
-# exports.createVRNCheckOut = function (data) {
-#   var checkOut = {
-#     VRNNum: data.VRN.toString(),
-#     VRNCREHRDITMNAV: [{
-#       CheckType: "O",
-#       DepRemarks: data.REMARKS,
-#       LRDate: "0000-00-00T00:00:00",
-#       LRNum: '',
-#       NoHus: data.NUMOFBOXES,
-#       Reject: "",
-#       SealCond: data.SEALCONDITION,
-#       TripNum: "",
-#       VRNCREITMDOCNAV: [{ DocNum: "", DocType: "" }],
-#       VRNNum: data.VRN.toString(),
-#       VehicleStatus: data.VEHICLESTATUS
-#     }]
-#   }
-#   postSAPData('/Z_FIORI_VRN_OUT_LITE_SRV/VRNCreHdrSet', checkOut);
-# }
-# 
-# exports.createVRNReortAndCheckIn = function createVRNReortAndCheckIn(data, ind) {
-#   var report = {
-#     Indicator: data.CHECKININD == 'X' ? 'X' : '',
-#     VRNCREHRDVEHNAV: [{
-#       VehicleNum: ind == 'X' ? data.VEHICLENUM : '',
-#       VendorNum: ind == 'X' ? data.TRANSPORTERCODE : '',
-#       FleetType: ind == 'X' ? data.FLEETTYPECODE : ''
-#     }],
-#     VRNHDRITEMNAV: [{
-#       CheckType: "I",
-#       Depremarks: data.REMARKS,
-#       Depseal: data.SEAL1,
-#       DriverName: data.DRIVERNAME,
-#       DriverNum: data.DRIVERNUM,
-#       FleetType: data.FLEETTYPECODE,
-#       IDPrfNum: data.IDPROOFNUM,
-#       IDPrfType: data.IDPROOFTYPE,
-#       LRDate: "0000-00-00T00:00:00",
-#       LRNum: data.LRNUM,
-#       LicenceNum: data.LICENSENUM,
-#       NoHus: data.NUMOFBOXES,
-#       Purpose: "VEND_INB",
-#       SealCond: data.SEALCONDITION,
-#       TCNNum: "",
-#       TransCode: data.TRANSPORTERCODE,
-#       Transporter: data.TRANSPORTER,
-#       VRNITEMDOCNAV: [{ DocType: "", DocNo: "" }],
-#       VehStatus: data.VEHICLESTATUS,
-#       VehicleNum: data.VEHICLENUM,
-#       VehicleType: data.MODEOFTRANSPORT
-#     }]
-#   }
-#   postSAPData('/Z_FIORI_VRN_IN_LITE_SRV/VRNCreateHdrSet', report);
-# }
-# 
-# exports.createVRNCheckIn = function (vrn) {
-#   var CheckData = {
-#     Indicator: "X",
-#     VRNNum: vrn.toString()
-#   }
-#   postSAPData('/Z_FIORI_VRN_IN_LITE_SRV/CheckInSet', CheckData);
-# }
